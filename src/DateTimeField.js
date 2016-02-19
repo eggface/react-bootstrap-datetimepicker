21990a818ed292df9ae58a017fff56e8fcba8dd7
Richard Lopes <richard.lopes@myob.com>
2016-02-19 17:23:57 +1300

    Fixed a bug when selecting a month, no resetting the error styles of the input.

--- src/DateTimeField.js
+++ src/DateTimeField.js
@@ -130,6 +130,7 @@ export default class DateTimeField extends Component {
   setSelectedMonth = (e) => {
     const { target } = e;
     if (target.className && !target.className.match(/disabled/g)) {
+      this.setIsValid(true);
       return this.setState({
         selectedDate: moment(this.state.viewDate.clone().toDate())
           .month(e.target.innerHTML).date(1)
