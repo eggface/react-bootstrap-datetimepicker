a920cd04ea5c88c7ffd5401b697e512a49254adc
Walid Halabi <walid.halabi@myob.com>
2016-11-07 13:15:49 +1100

    improve test legibility

--- src/__tests__/DateTimeField-test.js
+++ src/__tests__/DateTimeField-test.js
@@ -24,16 +24,18 @@ describe("DateTimeField", function () {
       expect(input.value).toBe("06/05/90 7:30 AM");
     });
 
-    it("initialises to a provided dateTime with a defaultText set", function () {
-      const component = TestUtils.renderIntoDocument(<DateTimeField defaultText="Foo" dateTime={happyDate.format("x")} />);
-      const input = TestUtils.findRenderedDOMComponentWithTag(component, "input");
-      expect(input.value).toBe("06/05/90 7:30 AM");
-    });
+    describe("when the defaultText prop is set", function() {
+      it("initialises the value to a provided dateTime prop", function () {
+        const component = TestUtils.renderIntoDocument(<DateTimeField defaultText="Foo" dateTime={happyDate.format("x")} />);
+        const input = TestUtils.findRenderedDOMComponentWithTag(component, "input");
+        expect(input.value).toBe("06/05/90 7:30 AM");
+      });
 
-    it("initialises to a blank string with a defaultText set and a blank dateTime", function () {
-      const component = TestUtils.renderIntoDocument(<DateTimeField defaultText="Foo" dateTime="" />);
-      const input = TestUtils.findRenderedDOMComponentWithTag(component, "input");
-      expect(input.value).toBe("");
+      it("initialises the value to a blank string with an empty string dateTime", function () {
+        const component = TestUtils.renderIntoDocument(<DateTimeField defaultText="Foo" dateTime="" />);
+        const input = TestUtils.findRenderedDOMComponentWithTag(component, "input");
+        expect(input.value).toBe("");
+      });
     });
 
   });
